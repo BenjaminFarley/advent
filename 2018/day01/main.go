df30f045bc2898f5a4a5f370dacec2e6fb67e995
Benjamin Farley <Ben.Farley@capgemini.com>
2018-12-27 16:34:07 +0000

    Part 2 - Complete

--- 2018/day01/main.go
+++ 2018/day01/main.go
@@ -9,36 +9,75 @@ import (
 )
 
 func main() {
-	inputS := openFileAndParse("inputs/Part1Input")
+	// Part 1
+	fmt.Println("Starting Part 2")
+	input1S := openFileAndParse("inputs/Part2Input")
 	// Set up results value
 	frequencyResults := 0
 
 	// Iterate over slice of string, converting each
 	// value into an int and adding value to results
-	for _, f := range inputS {
-		change, err := strconv.Atoi(f)
-		if err != nil {
-			log.Fatal(err)
-		}
-		frequencyResults += change
+	for _, f := range input1S {
+		frequencyResults += f
 	}
 
 	// Print out result
-	fmt.Println("Advent of Code - Day 1")
+	fmt.Println("Advent of Code - Day 1 - Part 1")
 	fmt.Println(frequencyResults)
+
+	// Part 2
+	fmt.Println("Starting Part 2")
+	input2S := openFileAndParse("inputs/Part2Input")
+	fmt.Println("Advent of Code - Day 1 - Part 2")
+	findDuplicate(input2S)
 
 }
 
 // openFileAndParse takes a filePath to a file that contains
-// frequencies and returns a slice of string
-func openFileAndParse(filePath string) []string {
+// frequencies and converts them into a slice of int
+func openFileAndParse(filePath string) []int {
 	// Load in File
-	inputB, err := ioutil.ReadFile(filePath)
+	file, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Transform slice of bytes to string and Split into
 	// a slice of string
-	return strings.Split(string(inputB), "\n")
+	listOfStrings := strings.Split(string(file), "\n")
+
+	var numbers []int
+	for _, n := range listOfStrings {
+		num, err := strconv.Atoi(n)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, num)
+	}
+	return numbers
+}
+
+// findDuplicate takes and parses a slice of frequencies
+// to find the first duplicate value
+func findDuplicate(input []int) int {
+
+	// Set up results value
+	frequencyResult := 0
+
+	foundDuplicate := map[int]bool{0: true}
+
+	// Iterate over input frequencies and check to see if
+	// the value is already stored in frequencyResults. If
+	// not, then append the result in frequencyResults.
+	for {
+		for _, n := range input {
+			frequencyResult += n
+			if foundDuplicate[frequencyResult] {
+				fmt.Println("Found a duplicate")
+				fmt.Println(frequencyResult)
+				return frequencyResult
+			}
+			foundDuplicate[frequencyResult] = true
+		}
+	}
 }
